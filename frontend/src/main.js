b3fddb844536b84b1e5cdbb7c8746ec2883c6347
Hosein Norouzi <hosein.norouzi76@gmail.com>
2018-07-25 19:33:46 +0430

    refactor(soundlite): animations are in a seperated file

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import Vue from 'vue'
 import './helper/style/reset.css'
 import './helper/style/main.css'
 import './helper/style/global.css'
+import './helper/style/animation.css'
+
 // components
 import App from './App.vue'
 
